c125f16e8157147962366796dc923e15dca52027
yuki-1184 <yuki.1184apple@gmail.com>
2023-07-11 14:48:35 +0900

    fetching partially success, needs more

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Items from './pages/Items'
+import ItemDetail from './pages/ItemDetail'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/items" replace />}></Route>
+        <Route path="/items" element={<Items></Items>}></Route>
+        <Route path="/items:id" element={<ItemDetail></ItemDetail>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
